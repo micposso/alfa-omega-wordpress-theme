4bb623d80c754a3d75931ec960ab81e3ffdf2880
micposso <micposso@gmail.com>
2022-07-05 16:24:32 -0400

    set up files structure

--- header.php
+++ header.php
@@ -34,3 +34,4 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 		<?php get_template_part( 'global-templates/navbar', $navbar_type . '-' . $bootstrap_version ); ?>
 
 	</header><!-- #wrapper-navbar end -->
+ 
